54aa21fa440b8ba73c339929610835a79186a7ce
Abdelghfar Salah <abdelghfarkhairallah8@gmail.com>
2026-09-11 15:26:46 +0300

    make header responsive

--- components/pages/home/header/HeaderLocation/index.tsx
+++ components/pages/home/header/HeaderLocation/index.tsx
@@ -1,7 +1,7 @@
 
 
 export default function HeaderLocation(){
-    return <div className="flex gap-2 items-center">
+    return <div className="flex gap-2 items-center hidden xl:flex">
         <p className="text-18 font-semibold text-gray-800" >Deliver to:</p>
         <img src={"/assets/icons/map-marker-alt.svg"} alt={"AppIcons not found"} width={14} height={25} />
         <p className="text-18 font-thin  text-gray-800" >Current Location</p>
